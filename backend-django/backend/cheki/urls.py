ea9943738881abacad6b84979ecde0e58d432f0b
Lakshay190202 <78367738+Lakshay190202@users.noreply.github.com>
2024-09-14 03:31:14 +0530

    fixed cheki/views.py

--- backend-django/backend/cheki/urls.py
+++ backend-django/backend/cheki/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('signin/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('signin/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signout/', CustomTokenBlackListView.as_view(), name='auth_signout'),
+    path('signout/', CustomTokenBlackListView.as_view(), name='auth_sign-out'),
     path('du/<int:user_id>/', DeleteUser.as_view(), name='delete_user'),
     
     ]
--- backend-django/backend/travelsite/settings.py
+++ backend-django/backend/travelsite/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import environ
 from datetime import timedelta
@@ -79,8 +78,6 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://192.168.29.150:3000',
-    'http://192.168.29.136:3000'
 
 ]
 CORS_ALLOW_CREDENTIALS = True
